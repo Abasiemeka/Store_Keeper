d14c25350a770d84490440b2e87f89ff2fb6b2b5
Abasiemeka <abasiemeka@gmail.com>
2023-10-09 16:50:37 +0100

    A store management application created during my Java training.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Person p = new Person();
-        //p.dateOfBirth = new Date(1986, Calendar.NOVEMBER, 4);
+        //p.dateOfBirth = new Date(1986, Calendar.NOVEMBER, 4); learn dates in java
         System.out.println(p.getDateOfBirth());
         System.out.println(p.getAge());
     }
