29ae1a7b2ed217afcb9c1210f6bc2b7dad56f924
Abasiemeka <abasiemeka@gmail.com>
2023-10-15 05:22:23 +0100

    A store management application created during my Java training.

--- src/main/java/Entities/Inventory.java
+++ src/main/java/Entities/Inventory.java
@@ -1,23 +1,22 @@
 package Entities;
 
-import javax.swing.*;
 import java.util.ArrayList;
 
 public final class Inventory {
-    private static ArrayList<String> inventoryData = new ArrayList<String>();
-    private static String inventoryFileName = "C:\\Users\\Abasiemeka\\IdeaProjects\\Store_Keeper\src\main\resources\Inventory.xlsx";
+    private static ArrayList<String> inventoryContents = new ArrayList<String>();
+    private static String inventoryFileName = "C:\\Users\\Abasiemeka\\IdeaProjects\\Store_Keeper\\src\\main\\resources\\Inventory.xlsx";
 
-    public static ArrayList<String> getInventoryData() {
-        return inventoryData;
+    public static ArrayList<String> getInventoryContents() {
+        return inventoryContents;
     }
 
     public static void addToInventory(String productName) {
-        inventoryData.add(productName);
+        inventoryContents.add(productName);
         System.out.println(productName + " has been added to store inventory.");
     }
 
     public static void removeFromInventory(String productName) {
-        inventoryData.remove(productName);
+        inventoryContents.remove(productName);
         System.out.println(productName + " has been removed from store inventory.");
     }
 
