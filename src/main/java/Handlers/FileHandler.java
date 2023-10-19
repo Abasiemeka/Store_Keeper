c39b419a8c6d48197a5d9a94b74ece374e4c1db5
Abasiemeka <abasiemeka@gmail.com>
2023-10-19 17:14:48 +0100

    A store management application created during my Java training.

--- src/main/java/Handlers/FileHandler.java
+++ src/main/java/Handlers/FileHandler.java
@@ -1,21 +1,60 @@
 package Handlers;
 
 
-import javax.swing.*;
-import java.io.FileInputStream;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public interface FileHandler {
     default void createFile(String filename) throws IOException {
         Path filePath = Path.of(filename);
         Files.createFile(filePath);
     }
-    default void openFile(String filename) throws FileNotFoundException {
-
+    default void openFile(Files file) throws FileNotFoundException {
+        Files.newInputStream(file);
     }
-    void readFile()
+    default void readFile(){
+    }
+
+    XSSFWorkbook workbook = new XSSFWorkbook();
+    XSSFSheet spreadsheet = workbook.createSheet("employe db");
+
+    XSSFRow row = spreadsheet.createRow(1);
+    XSSFCell cell;
+    cell = row.createCell(1);
+      cell.setCellValue("EMP ID");
+    cell = row.createCell(2);
+      cell.setCellValue("EMP NAME");
+    cell = row.createCell(3);
+      cell.setCellValue("DEG");
+    cell = row.createCell(4);
+      cell.setCellValue("SALARY");
+    cell = row.createCell(5);
+      cell.setCellValue("DEPT");
+    int i = 2;
+
+      while(resultSet.next()) {
+        row = spreadsheet.createRow(i);
+        cell = row.createCell(1);
+        cell.setCellValue(resultSet.getInt("eid"));
+        cell = row.createCell(2);
+        cell.setCellValue(resultSet.getString("ename"));
+        cell = row.createCell(3);
+        cell.setCellValue(resultSet.getString("deg"));
+        cell = row.createCell(4);
+        cell.setCellValue(resultSet.getString("salary"));
+        cell = row.createCell(5);
+        cell.setCellValue(resultSet.getString("dept"));
+        i++;
+    }
+
+    FileOutputStream out = new FileOutputStream(new File("exceldatabase.xlsx"));
+      workbook.write(out);
+      out.close();
+      System.out.println("exceldatabase.xlsx written successfully");
+}
 }
--- src/main/java/Transactions/Sales.java
+++ src/main/java/Transactions/Sales.java
@@ -3,3 +3,29 @@ package Transactions;
 public class Sales {
     //todo: implement methods to sell, buy, discount, refund
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
